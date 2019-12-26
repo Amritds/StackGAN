e1c6ca1d8fe98e51e497a944d3486e447bb6b9ef
Amritds <amrit.seshadri@ushur.com>
2019-12-26 15:29:14 +0530

    Remove training and load model in constructor

--- code/trainer.py
+++ code/trainer.py
@@ -45,6 +45,12 @@ class GANTrainer(object):
         torch.cuda.set_device(self.gpus[0])
         cudnn.benchmark = True
 
+        if stage == 1:
+            self.NetG, _ = self.load_network_stageI()
+        else:
+            self.NetG, _ = self.load_network_stageII()
+        self.NetG.eval()
+        
     # ############# For training stageI GAN #############
     def load_network_stageI(self):
         from model import STAGE1_G, STAGE1_D
@@ -111,133 +117,10 @@ class GANTrainer(object):
             netD.cuda()
         return netG, netD
 
-    def train(self, data_loader, stage=1):
-        if stage == 1:
-            netG, netD = self.load_network_stageI()
-        else:
-            netG, netD = self.load_network_stageII()
-
-        nz = cfg.Z_DIM
-        batch_size = self.batch_size
-        noise = Variable(torch.FloatTensor(batch_size, nz))
-        fixed_noise = \
-            Variable(torch.FloatTensor(batch_size, nz).normal_(0, 1),
-                     volatile=True)
-        real_labels = Variable(torch.FloatTensor(batch_size).fill_(1))
-        fake_labels = Variable(torch.FloatTensor(batch_size).fill_(0))
-        if cfg.CUDA:
-            noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
-            real_labels, fake_labels = real_labels.cuda(), fake_labels.cuda()
-
-        generator_lr = cfg.TRAIN.GENERATOR_LR
-        discriminator_lr = cfg.TRAIN.DISCRIMINATOR_LR
-        lr_decay_step = cfg.TRAIN.LR_DECAY_EPOCH
-        optimizerD = \
-            optim.Adam(netD.parameters(),
-                       lr=cfg.TRAIN.DISCRIMINATOR_LR, betas=(0.5, 0.999))
-        netG_para = []
-        for p in netG.parameters():
-            if p.requires_grad:
-                netG_para.append(p)
-        optimizerG = optim.Adam(netG_para,
-                                lr=cfg.TRAIN.GENERATOR_LR,
-                                betas=(0.5, 0.999))
-        count = 0
-        for epoch in range(self.max_epoch):
-            start_t = time.time()
-            if epoch % lr_decay_step == 0 and epoch > 0:
-                generator_lr *= 0.5
-                for param_group in optimizerG.param_groups:
-                    param_group['lr'] = generator_lr
-                discriminator_lr *= 0.5
-                for param_group in optimizerD.param_groups:
-                    param_group['lr'] = discriminator_lr
-
-            for i, data in enumerate(data_loader, 0):
-                ######################################################
-                # (1) Prepare training data
-                ######################################################
-                real_img_cpu, txt_embedding = data
-                real_imgs = Variable(real_img_cpu)
-                txt_embedding = Variable(txt_embedding)
-                if cfg.CUDA:
-                    real_imgs = real_imgs.cuda()
-                    txt_embedding = txt_embedding.cuda()
-
-                #######################################################
-                # (2) Generate fake images
-                ######################################################
-                noise.data.normal_(0, 1)
-                inputs = (txt_embedding, noise)
-                _, fake_imgs, mu, logvar = \
-                    nn.parallel.data_parallel(netG, inputs, self.gpus)
-
-                ############################
-                # (3) Update D network
-                ###########################
-                netD.zero_grad()
-                errD, errD_real, errD_wrong, errD_fake = \
-                    compute_discriminator_loss(netD, real_imgs, fake_imgs,
-                                               real_labels, fake_labels,
-                                               mu, self.gpus)
-                errD.backward()
-                optimizerD.step()
-                ############################
-                # (2) Update G network
-                ###########################
-                netG.zero_grad()
-                errG = compute_generator_loss(netD, fake_imgs,
-                                              real_labels, mu, self.gpus)
-                kl_loss = KL_loss(mu, logvar)
-                errG_total = errG + kl_loss * cfg.TRAIN.COEFF.KL
-                errG_total.backward()
-                optimizerG.step()
-
-                count = count + 1
-                if i % 100 == 0:
-                    summary_D = summary.scalar('D_loss', errD.data[0])
-                    summary_D_r = summary.scalar('D_loss_real', errD_real)
-                    summary_D_w = summary.scalar('D_loss_wrong', errD_wrong)
-                    summary_D_f = summary.scalar('D_loss_fake', errD_fake)
-                    summary_G = summary.scalar('G_loss', errG.data[0])
-                    summary_KL = summary.scalar('KL_loss', kl_loss.data[0])
-
-                    self.summary_writer.add_summary(summary_D, count)
-                    self.summary_writer.add_summary(summary_D_r, count)
-                    self.summary_writer.add_summary(summary_D_w, count)
-                    self.summary_writer.add_summary(summary_D_f, count)
-                    self.summary_writer.add_summary(summary_G, count)
-                    self.summary_writer.add_summary(summary_KL, count)
-
-                    # save the image result for each epoch
-                    inputs = (txt_embedding, fixed_noise)
-                    lr_fake, fake, _, _ = \
-                        nn.parallel.data_parallel(netG, inputs, self.gpus)
-                    save_img_results(real_img_cpu, fake, epoch, self.image_dir)
-                    if lr_fake is not None:
-                        save_img_results(None, lr_fake, epoch, self.image_dir)
-            end_t = time.time()
-            print('''[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f Loss_KL: %.4f
-                     Loss_real: %.4f Loss_wrong:%.4f Loss_fake %.4f
-                     Total Time: %.2fsec
-                  '''
-                  % (epoch, self.max_epoch, i, len(data_loader),
-                     errD.data[0], errG.data[0], kl_loss.data[0],
-                     errD_real, errD_wrong, errD_fake, (end_t - start_t)))
-            if epoch % self.snapshot_interval == 0:
-                save_model(netG, netD, epoch, self.model_dir)
-        #
-        save_model(netG, netD, self.max_epoch, self.model_dir)
-        #
-        self.summary_writer.close()
+   
 
     def sample(self, datapath, stage=2):
-        if stage == 1:
-            netG, _ = self.load_network_stageI()
-        else:
-            netG, _ = self.load_network_stageII()
-        netG.eval()
-
+        
         # Load text embeddings generated from the encoder
         t_file = torchfile.load(datapath)
         captions_list = t_file.raw_txt
@@ -275,7 +158,7 @@ class GANTrainer(object):
             noise.data.normal_(0, 1)
             inputs = (txt_embedding, noise)
             _, fake_imgs, mu, logvar = \
-                nn.parallel.data_parallel(netG, inputs, self.gpus)
+                nn.parallel.data_parallel(self.NetG, inputs, self.gpus)
             for i in range(batch_size):
                 save_name = '%s/%d.jpg' % (save_dir, count + i)
                 im = fake_imgs[i].data.cpu().numpy()
