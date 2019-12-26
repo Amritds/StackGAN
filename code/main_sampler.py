f2b4b10c17bae7fe9644d62fb16100725e351095
Amritds <amrit.seshadri@ushur.com>
2019-12-26 15:26:32 +0530

    remove training and argparser

---
+++ code/main_sampler.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import torch.backends.cudnn as cudnn
+import torch
+import torchvision.transforms as transforms
+
+import argparse
+import os
+import random
+import sys
+import pprint
+import datetime
+import dateutil
+import dateutil.tz
+
+
+dir_path = (os.path.abspath(os.path.join(os.path.realpath(__file__), './.')))
+sys.path.append(dir_path)
+
+from miscc.datasets import TextDataset
+from miscc.config import cfg, cfg_from_file
+from miscc.utils import mkdir_p
+from trainer import GANTrainer
+
+
+cfg_from_file('cfg/coco_eval.yml')
+cfg.GPU_ID ='0'
+cfg.DATA_DIR = ''
+    
+print('Using config:')
+pprint.pprint(cfg)
+    
+manualSeed = random.randint(1, 10000)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+if cfg.CUDA:
+    torch.cuda.manual_seed_all(manualSeed)
+now = datetime.datetime.now(dateutil.tz.tzlocal())
+timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+output_dir = '../output/%s_%s_%s' % \
+             (cfg.DATASET_NAME, cfg.CONFIG_NAME, timestamp)
+
+num_gpu = len(cfg.GPU_ID.split(','))
+
+datapath= '%s' % (cfg.DATA_PATH)
+algo = GANTrainer(output_dir)
+
+def sample():
+    algo.sample(datapath, cfg.STAGE)
